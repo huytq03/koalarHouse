219023b3e0d1228bf26392210fbde0c42b5551cc
huy tran <trquochuy312@huys-MacBook-Pro.local>
2016-07-20 00:19:49 +0700

    update morepageç

--- wordpress/wp-content/themes/twentysixteen/template-parts/content-single.php
+++ wordpress/wp-content/themes/twentysixteen/template-parts/content-single.php
@@ -9,9 +9,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+
 	
 	<div class="entry-content">
 		<?php
@@ -30,6 +28,26 @@
 				get_template_part( 'template-parts/biography' );
 			}
 		?>
+
 	</div><!-- .entry-content -->
+	<div class="related-post">
+		<h4>Các tin khác:</h4>
+
+		 <ul> 
+		<?php
+
+		$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 5, 'post__not_in' => array($post->ID) ) );
+		if( $related ) {
+			foreach( $related as $post ) {
+		 ?>
+		        <li>
+		        <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+		            
+		        </li>
+		 
+		<?php } }
+		wp_reset_postdata(); ?>
+		    </ul>  
+	</div><!-- # .orther-post-## -->
 
 </article><!-- #post-## -->
